23238e263898ad11cfca847b348322225b2f9924
Maksim <makstanc@yandex.ru>
2024-05-13 02:39:25 +0500

    ready authorisate and registration, remove bug with css

--- app1/app/settings.py
+++ app1/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from django.conf.global_settings import STATICFILES_DIRS
 
@@ -124,6 +125,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
     ]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+#STATIC_ROOT = (os.path.dirname(BASE_DIR), "static") 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
